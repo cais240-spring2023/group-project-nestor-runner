82422e30cf433e462f8ab65f55888dc7dfe278af
Glenn <glane6933@westfield.ma.edu>
2023-02-17 13:25:03 -0500

    Updated the App.java to have loaders/panes and load the menu/game properly including switching between the two.

--- src/main/java/edu/wsu/App.java
+++ src/main/java/edu/wsu/App.java
@@ -1,9 +1,11 @@
 package edu.wsu;
 
+import edu.wsu.model.NestorRunnerSingleton;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,23 +14,43 @@ import java.io.IOException;
  * JavaFX App
  */
 public class App extends Application {
-
+    private static FXMLLoader menuLoader = new FXMLLoader(App.class.getResource("menu.fxml"));
+    private static FXMLLoader gameLoader = new FXMLLoader(App.class.getResource("game.fxml"));
     private static Scene scene;
+    private static StackPane gamePane;
+    private static StackPane menuPane;
+
+    static {
+        try {
+            menuPane = menuLoader.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    static {
+        try {
+            gamePane = gameLoader.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("game"), 640, 480);
+        NestorRunnerSingleton.getInstance().setMenuView(menuLoader.getController());
+        NestorRunnerSingleton.getInstance().setGameView(gameLoader.getController());
+        scene = new Scene(menuPane, 640, 480);
         stage.setScene(scene);
         stage.show();
     }
 
     public static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    public static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
+        if(fxml.equals("game")) {
+            scene.setRoot(gamePane);
+        } else if(fxml.equals("menu")) {
+            scene.setRoot(menuPane);
+        }
     }
 
     public static void main(String[] args) {
