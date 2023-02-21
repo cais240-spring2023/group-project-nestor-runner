84189bea1b767716c158965b2554331f0b2539d7
Glenn <glane6933@westfield.ma.edu>
2023-02-21 16:00:54 -0500

    Added all fxml objects to the controllers

--- src/main/java/edu/wsu/view/GameViewImpl.java
+++ src/main/java/edu/wsu/view/GameViewImpl.java
@@ -1,9 +1,11 @@
 package edu.wsu.view;
 
+import javafx.fxml.FXML;
 import javafx.scene.layout.StackPane;
 
 public class GameViewImpl implements GameView {
 
+    @FXML
     private StackPane endScreen;
 
     @Override
